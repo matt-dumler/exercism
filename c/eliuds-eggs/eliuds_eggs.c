47503d5bef85a199ae99bff53ae3eb750edf66a0
Matt Dumler <mattd3v@protonmail.com>
2024-07-08 13:39:21 -0500

    add c/eliuds-eggs exercise

---
+++ c/eliuds-eggs/eliuds_eggs.c
@@ -0,0 +1,20 @@
+#include "eliuds_eggs.h"
+
+/*
+ * egg_count: returns the number of 1's in the binary representation of an
+ *     integer.
+ */
+unsigned int egg_count(unsigned int input)
+{
+    int count = 0;
+    while (input > 0) {
+        // Increment the counter if the least significant bit is one.
+        if (input & 1) {
+            count++;
+        }
+        // Shift all bits to the right one, so we can process the next bit.
+        input >>= 1;
+    }
+
+    return count;
+}
