1cf90700b7e5834b4a4ea14e5b91ede36a9d8d60
Matt Dumler <mattd3v@protonmail.com>
2024-07-12 14:44:40 -0500

    update c/resistor-color-trio exercise

--- c/resistor-color-trio/resistor_color_trio.c
+++ c/resistor-color-trio/resistor_color_trio.c
@@ -1,10 +1,53 @@
 #include "resistor_color_trio.h"
 
 #define TENS_PLACE 10
-#define HUNDREDS_PLACE 100
 
 // color_code: converts three resistor colors to a numerical value
-uint8_t color_code(resistor_band_t colors[])
+resistor_value_t color_code(resistor_band_t colors[])
 {
-    return (colors[0] * HUNDREDS_PLACE) + (colors[1] * TENS_PLACE) + colors[2];
+    struct resistor_value_t result;
+
+    result.value = (10 * colors[0]) + colors[1];
+
+    resistor_band_t color = colors[2];
+    switch (color) {
+        case BLACK:
+            result.unit = OHMS;
+            break;
+        case BROWN:
+            result.unit = OHMS;
+            result.value *= 10;
+            break;
+        case RED:
+            result.unit = KILOOHMS;
+            result.value /= 10;
+            break;
+        case ORANGE:
+            result.unit = KILOOHMS;
+            break;
+        case YELLOW:
+            result.unit = KILOOHMS;
+            result.value *= 10;
+            break;
+        case GREEN:
+            result.unit = KILOOHMS;
+            result.value *= 100;
+            break;
+        case BLUE:
+            result.unit = MEGAOHMS;
+            break;
+        case VIOLET:
+            result.unit = MEGAOHMS;
+            result.value *= 10;
+            break;
+        case GREY:
+            result.unit = MEGAOHMS;
+            result.value *= 100;
+            break;
+        case WHITE:
+            result.unit = GIGAOHMS;
+            break;
+    }
+
+    return result;
 }
