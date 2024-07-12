cef7b7b50a0b1a3569cfc28e00d99d5a78e06e80
Matt Dumler <mattd3v@protonmail.com>
2024-07-12 17:55:13 -0500

    update cpp/lasagna exercise

--- cpp/lasagna/lasagna_test.cpp
+++ cpp/lasagna/lasagna_test.cpp
@@ -14,10 +14,8 @@ TEST_CASE("Preparation time correct", "[task_1]") {
     REQUIRE(expected == actual);
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
-
 TEST_CASE("Fresh in the oven", "[task_2]") {
-    int timeSpendInOven = 0;
+    constexpr int timeSpendInOven = 0;
     int neededBakeTime = 40;
     int actual = remainingOvenTime(timeSpendInOven);
     int expected{neededBakeTime - timeSpendInOven};
@@ -26,7 +24,7 @@ TEST_CASE("Fresh in the oven", "[task_2]") {
 }
 
 TEST_CASE("Halfway done", "[task_2]") {
-    int timeSpendInOven = 20;
+    constexpr int timeSpendInOven = 20;
     int neededBakeTime = 40;
     int actual = remainingOvenTime(timeSpendInOven);
     int expected{neededBakeTime - timeSpendInOven};
@@ -36,7 +34,7 @@ TEST_CASE("Halfway done", "[task_2]") {
 
 TEST_CASE("Correct for six layers", "[task_3]") {
     int timePerLayer = 2;
-    int layers = 6;
+    constexpr int layers = 6;
     int actual = preparationTime(layers);
     int expected{timePerLayer * layers};
 
@@ -45,7 +43,7 @@ TEST_CASE("Correct for six layers", "[task_3]") {
 
 TEST_CASE("Correct for 11 layers", "[task_3]") {
     int timePerLayer = 2;
-    int layers = 11;
+    constexpr int layers = 11;
     int actual = preparationTime(layers);
     int expected{timePerLayer * layers};
 
@@ -53,9 +51,9 @@ TEST_CASE("Correct for 11 layers", "[task_3]") {
 }
 
 TEST_CASE("Fresh in the oven, 12 layers!", "[task_4]") {
-    int timeSpendInOven = 0;
+    constexpr int timeSpendInOven = 0;
     int timePerLayer = 2;
-    int layers = 12;
+    constexpr int layers = 12;
     int actual = elapsedTime(layers, timeSpendInOven);
     int expected{timePerLayer * layers + timeSpendInOven};
 
@@ -63,13 +61,12 @@ TEST_CASE("Fresh in the oven, 12 layers!", "[task_4]") {
 }
 
 TEST_CASE("One minute left, 5 layers!", "[task_4]") {
-    int timeSpendInOven = 39;
+    constexpr int timeSpendInOven = 39;
     int timePerLayer = 2;
-    int layers = 5;
+    constexpr int layers = 5;
     int actual = elapsedTime(layers, timeSpendInOven);
     int expected{timePerLayer * layers + timeSpendInOven};
 
     REQUIRE(expected == actual);
 }
 
-#endif
