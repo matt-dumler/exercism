cbc423dd6af828a6ece8191bc86cbcac9a7f5fb6
Matt Dumler <mattd3v@pm.me>
2024-07-27 21:56:04 -0500

    add c/bob exercise

---
+++ c/bob/bob.c
@@ -0,0 +1,58 @@
+#include "bob.h"
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+
+static bool is_silence(char *greeting)
+{
+    for (char *p = greeting; *p != '\0'; p++) {
+        if (!isspace(*p)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+static bool is_question(char *greeting)
+{
+    char *p = greeting + (strlen(greeting) - 1);
+
+    while (isspace(*p))
+        p--;
+
+    return *p == '?';
+}
+
+static bool is_shouted(char *greeting)
+{
+    bool is_shouted = false;
+    for (char *p = greeting; *p != '\0'; p++) {
+
+        if (isalpha(*p)) {
+            is_shouted = true;
+
+            if (!isupper(*p)) {
+                return false;
+            }
+        }
+    }
+
+    return is_shouted;
+}
+
+char *hey_bob(char *greeting)
+{
+    if (is_silence(greeting)) {
+        return "Fine. Be that way!";
+    } else if (is_question(greeting) && is_shouted(greeting)) {
+        return "Calm down, I know what I'm doing!";
+    } else if (is_question(greeting)) {
+        return "Sure.";
+    } else if (is_shouted(greeting)) {
+        return "Whoa, chill out!";
+    } else {
+        return "Whatever.";
+    }
+}
