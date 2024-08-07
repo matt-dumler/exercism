37a6f9fbf1ad70a91ac3bb56f91462cd6875b0dd
Matt Dumler <mattd3v@protonmail.com>
2024-08-07 13:27:28 -0500

    update go/blackjack exercise

--- go/blackjack/blackjack.go
+++ go/blackjack/blackjack.go
@@ -2,11 +2,54 @@ package blackjack
 
 // ParseCard returns the integer value of a card following blackjack ruleset.
 func ParseCard(card string) int {
-	panic("Please implement the ParseCard function")
+	switch card {
+    case "ace":
+        return 11
+    case "two":
+        return 2
+    case "three":
+        return 3
+    case "four":
+        return 4
+    case "five":
+        return 5
+    case "six":
+        return 6
+    case "seven":
+        return 7
+    case "eight":
+        return 8
+    case "nine":
+        return 9
+    case "ten", "jack", "queen", "king":
+        return 10
+    default:
+        return 0
+    }
 }
 
 // FirstTurn returns the decision for the first turn, given two cards of the
 // player and one card of the dealer.
 func FirstTurn(card1, card2, dealerCard string) string {
-	panic("Please implement the FirstTurn function")
+    sum := ParseCard(card1) + ParseCard(card2)
+    dealer := ParseCard(dealerCard)
+
+    switch {
+    case sum == 22:
+        return "P"
+    case sum == 21:
+        if dealer < 10 {
+            return "W"
+        }
+        return "S"
+    case sum >= 17 && sum < 21:
+        return "S"
+    case sum > 11 && sum < 17:
+        if dealer > 6 {
+            return "H"
+        }
+        return "S"
+    default:
+        return "H"
+    }
 }
